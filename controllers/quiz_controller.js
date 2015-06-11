ded32e481e06e08bc3ca741eafdec412ff93660f
vialhue <vialhue@gmail.com>
2015-06-11 23:45:51 +0200

    Primera pregunta

---
+++ controllers/quiz_controller.js
@@ -0,0 +1,13 @@
+// método GET para las /quizes/question
+exports.question = function(req, res) {
+  res.render('quizes/question', {pregunta : 'Capital de Italia'});
+};
+
+// método GET para las /quizes/answer
+exports.answer = function(req, res) {
+  if (req.query.respuesta === 'Roma') {
+      res.render('quizes/answer', {respuesta : 'Correcto'});
+  } else {
+      res.render('quizes/answer', {respuesta : 'Incorrecto'});
+  }
+};
