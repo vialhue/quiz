1b0b39dec9cdcb36c5a6a0cfea92b6476ca47aff
vialhue <vialhue@gmail.com>
2015-06-29 21:07:10 +0200

    Crear preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -45,3 +45,20 @@ exports.answer = function(req, res) {
     }
     res.render('quizes/answer', { quiz : req.quiz, respuesta : resultado});
 };
+
+// GET /quizes/new
+exports.new = function(req, res) {
+    var quiz = models.Quiz.build(
+        {pregunta : "Pregunta", respuesta : "Respuesta"}
+    );
+    res.render('quizes/new', {quiz : quiz});
+};
+
+exports.create = function(req, res) {
+    var quiz = models.Quiz.build(req.body.quiz);
+    // Guarda la pregunta en la BBDD
+    quiz.save({fields: ["pregunta", "respuesta"]}).then(function() {
+        res.redirect('/quizes');
+    });
+};
+
