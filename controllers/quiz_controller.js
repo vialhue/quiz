e6b9216ca28dc3c905f647007fea56441028d9a0
vialhue <vialhue@gmail.com>
2015-07-05 21:57:15 +0200

    Editar

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -73,3 +73,27 @@ exports.create = function(req, res) {
     );
 };
 
+exports.edit = function(req, res) {
+    var quiz = req.quiz; // Viene del AUTOLOAD.
+    res.render('quizes/edit', { quiz : quiz, errors : []});
+};
+
+exports.update = function(req, res) {
+    req.quiz.pregunta = req.body.quiz.pregunta;
+    req.quiz.respuesta = req.body.quiz.respuesta;
+    
+    req.quiz
+    .validate()
+    .then(
+        function(err) {
+            if (err) {
+                res.render('quizes/edit', {quiz: req.quiz, errors: err.errors});
+            } else {
+                req.quiz
+                .save({fields: ["pregunta", "respuesta"]})
+                .then(function() { res.redirect('/quizes') });
+            }
+        }
+    )
+}
+
