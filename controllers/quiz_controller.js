d350ca28f417cafb30638a57b0705412bfce09d7
vialhue <vialhue@gmail.com>
2015-07-07 18:28:01 +0200

    Correccion temas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -80,6 +80,7 @@ exports.edit = function(req, res) {
 exports.update = function(req, res) {
     req.quiz.pregunta = req.body.quiz.pregunta;
     req.quiz.respuesta = req.body.quiz.respuesta;
+    req.quiz.tema = req.body.quiz.tema;
     
     req.quiz
     .validate()
